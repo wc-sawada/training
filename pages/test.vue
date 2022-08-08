069a657409634c4551f7d4222aa43dcaca52a976
澤田羽衣 <38057354+wc-sawada@users.noreply.github.com>
2022-08-08 09:14:14 +0900

    add dart-sass

---
+++ pages/test.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <p class="test_text">test</p>
+  </main>
+</template>
+<style lang="sass" scoped>
+.test_text 
+  font-size: 20px
+  font-weight: bold
+  color: red
+</style>
